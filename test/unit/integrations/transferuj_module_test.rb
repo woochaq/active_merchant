a355ea844ff15d0b890bf69c779e2f5cdca4fa1b
Piotr Misiurek <p.misiurek@gmail.com>
2012-03-16 17:36:27 +0100

    Revert "Merged integration for transferuj.pl"
    This reverts commit f065fc97b4fd3a91a0ebd488d0631f947f003947.

--- test/unit/integrations/transferuj_module_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TransferujModuleTest < Test::Unit::TestCase
-  include ActiveMerchant::Billing::Integrations
-  
-  def test_notification_method
-    assert_instance_of Transferuj::Notification, Transferuj.notification('name=cody', :secret => 'secret')
-  end
-end 
